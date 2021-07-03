5ae69953a3b2bb13a8783e834b44591fb5ef244c
Samuel Chen <68937749+Sparye@users.noreply.github.com>
2021-07-03 16:23:28 +1200

    added button support
    added button for enabling the function

--- popup.js
+++ popup.js
@@ -1,21 +1,21 @@
-chrome.storage.sync.get("color", ({ color }) => {
-  enhance.style.backgroundColor = color;
-});
+var backgroundPage;
+var theButton = document.querySelector("button");
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-enhance.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: startEnhancing,
-    });
-  });
-  
-  // The body of this function will be executed as a content script inside the
-  // current page
-  function startEnhancing() {
-    chrome.storage.sync.get("color", ({ color }) => {
-      document.body.style.backgroundColor = color;
-    });
-  }
+function updateButton(onOrOff) {
+  theButton.innerHTML = onOrOff ? "Disable" : "Enable";
+  theButton.className = onOrOff ? "button button3" : "button button1";
+}
+
+function toggleButton() {
+  backgroundPage.isExtensionOn = !backgroundPage.isExtensionOn;
+  updateButton(backgroundPage.isExtensionOn);
+}
+
+chrome.runtime.getBackgroundPage(function (bgPage) {
+  backgroundPage = bgPage;
+  updateButton(bgPage.isExtensionOn);
+  theButton.onclick = toggleButton;
+  theButton.style.display = "";
+  document.getElementById("br1").style.display = "";
+  document.getElementById("br1").style.display = "";
+});
