90c35c6124c2b35e49d140ec7a6f6094c51de2b2
Hiruna Jayamanne <hiru@hiru.dev>
2021-07-03 14:19:22 +1200

    Fix error around redirect rules

--- background.js
+++ background.js
@@ -3,10 +3,12 @@ const redirectTo = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 chrome.webRequest.onBeforeRequest.addListener(
     (d) => {
         console.log(d);
-        // if (d.initiator.indexOf("youtube") > -1 || d.url.indexOf("youtube") > -1 || d.url.indexOf("youtube") > -1) {
-        //     return {}
-        // }
-        if (d.url.indexOf("google.com") === -1 && d.initiator.indexOf("google.com" > -1) && d.url.indexOf("youtube.com") === -1) {
+        if (d.type === "image" || d.type === "font" || d.type === "video") {
+            return;
+        }
+        if (d.initiator.indexOf("google.com") > -1
+            && d.initiator.indexOf("youtube.com") === -1
+            && d.url.indexOf("youtube.com") === -1) {
             return {redirectUrl: redirectTo};
         }
     },
